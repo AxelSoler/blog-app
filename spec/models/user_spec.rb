5c9e956f5d528cb4d924b7ae6aa86dbbe5742435
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-10 18:50:04 -0300

    all files needed for tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #tests go here
+  subject { User.new(name: 'Axel', photo: 'https.something.com', bio: 'This is a good story', posts_counter: 2) }
+  
+  before { subject.save }
+
+  it 'name should be Axel' do
+    subject.name = 'Axel'
+  end
 end
