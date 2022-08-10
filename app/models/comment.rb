6648de51a4d10c06ebc7dfe5dd1ec41d334e3cab
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-10 14:59:09 -0300

    method that updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def updates_post_comments_counter
+    post.comments_couter.nil? ? post.update(comments_couter: 1) : post.update(comments_couter: post.posts_counter + 1)
+  end
 end
