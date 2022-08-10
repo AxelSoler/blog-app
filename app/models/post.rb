62fa023f78c2e06024b849a12fdc040931d1fb85
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-10 14:52:12 -0300

    posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def updates_user_posts_counter
+    author.posts_counter.nil? ? author.update(posts_counter: 1) : author.update(posts_counter: author.posts_counter + 1)
+  end
 end
