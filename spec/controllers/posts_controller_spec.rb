7947d88c7b29ca9fe19cd267213c43e1cdb5764c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-15 19:27:07 -0300

    tests for posts
    status and template

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET posts page #index' do
+    before(:each) do
+      get "/users/326/posts"
+    end
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+  end
+  describe 'Show the chosen post' do
+    before(:each) do
+      get "/users/326/posts/12"
+    end
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+  end
+end
