e939eb47b4a6a9e6ee7d5f7efb294f900ae4ee40
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-15 19:23:15 -0300

    testing if user/:id status and template was rendered

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET users page #index' do
-    it "is a success" do
+    before(:each) do
       get "/users"
+    end
+
+    it "is a success" do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
-      get "/users"
       expect(response).to render_template('index')
+    end
+  end
+  describe 'Show the chosen user' do
+    before(:each) do
+      get "/users/478"
+    end
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
     end
   end
 end
