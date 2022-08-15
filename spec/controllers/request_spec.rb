cf78e1673917619199982a62288b88d81f765dac
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-15 20:00:48 -0300

    changed the tests to one file as required

---
+++ spec/controllers/request_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET users page #index' do
+    before(:each) do
+      get '/users'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders index template' do
+      expect(response).to render_template('index')
+    end
+    # it 'body includes correct placeholder text' do
+    #   expect(response).to has_text('list of all users')
+    # end
+  end
+  describe 'Show the chosen user' do
+    before(:each) do
+      get '/users/478'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+  end
+end
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET posts page #index' do
+    before(:each) do
+      get '/users/326/posts'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+  end
+  describe 'Show the chosen post' do
+    before(:each) do
+      get '/users/326/posts/12'
+    end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+  end
+end
