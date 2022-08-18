3a1286c2ba5e7843b354b2bf8f6d6dc9923b0cf3
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-18 19:03:24 -0300

    form for new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,18 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-  # def create
-  #   @comment = Comment.new(author: current_user, text: post_params[:text])
-  #   if @comment.save
-  #     redirect_to user_posts_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @comment = Comment.new(author: current_user, post_id: params[:post_id], text: post_params[:text])
+    if @comment.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      render :new
+    end
+  end
 
-  # private
-  # def post_params
-  #   params.require(:comment).permit(:text)
-  # end
+  private
+
+  def post_params
+    params.require(:comment).permit(:text)
+  end
 end
