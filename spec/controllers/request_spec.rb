ef7483c39b4f3fc523b3be78d6e254db0cc2f259
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-22 18:22:23 -0300

    update old tests

--- spec/controllers/request_spec.rb
+++ spec/controllers/request_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before do
+    @user = User.create(name: 'Axel', photo: 'https.something.com', bio: 'This is a good story', posts_counter: 2)
+  end
+
   describe 'GET users page #index' do
     before(:each) do
-      get '/users'
+      get users_path
     end
 
     it 'is a success' do
@@ -13,12 +17,12 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template('index')
     end
     it 'body includes correct placeholder text' do
-      assert_select 'h2', 'list of all users'
+      assert_select 'h2', 'LIST OF USERS'
     end
   end
   describe 'Show the chosen user' do
     before(:each) do
-      get '/users/478'
+      get user_path(@user.id)
     end
 
     it 'is a success' do
@@ -28,15 +32,19 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template('show')
     end
     it 'body includes correct placeholder text' do
-      assert_select 'h2', 'selected user'
+      assert_select 'h2', 'USER'
     end
   end
 end
 
 RSpec.describe 'Posts', type: :request do
+  before do
+    @user = User.create(name: 'Axel', photo: 'https.something.com', bio: 'This is a good story', posts_counter: 2)
+    @post = Post.create(author: @user, title: 'Hello', text: 'testing posts', comments_counter: 0, likes_counter: 4)
+  end
   describe 'GET posts page #index' do
     before(:each) do
-      get '/users/326/posts'
+      get user_posts_path(@user.id)
     end
 
     it 'is a success' do
@@ -51,7 +59,7 @@ RSpec.describe 'Posts', type: :request do
   end
   describe 'Show the chosen post' do
     before(:each) do
-      get '/users/326/posts/12'
+      get user_post_path(@user.id, @post.id)
     end
 
     it 'is a success' do
@@ -61,7 +69,7 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('show')
     end
     it 'body includes correct placeholder text' do
-      assert_select 'h2', 'selected post'
+      assert_select 'h2', 'POST'
     end
   end
 end
