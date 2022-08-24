fee67f70415b76b9282d42fa1dc8c450d4ce2794
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-24 20:29:08 -0300

    mail confirmation and update database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,15 +45,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-  
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
+  
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
   
@@ -65,18 +59,22 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
-
+  
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
+  
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :user_name => '71971ac283227c',
