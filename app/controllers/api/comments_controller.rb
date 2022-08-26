83f4814a30cdfed9ff4462188441355fc36a3018
unknown <skj99667788@gmail.com>
2022-08-27 03:19:48 +0500

    comment controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,8 @@
+class Api::CommentsController < ApplicationController
+    def index
+      @post = Post.find(params[:post_id])
+      @comments = @post.comments
+      render json: @comments
+    end
+  end
+  
