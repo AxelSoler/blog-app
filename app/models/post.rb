62ffe7338b901b4f2716d6174b63fdc0941a0c99
unknown <skj99667788@gmail.com>
2022-08-26 20:27:56 +0500

    create the button to delete post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'post_id', dependent: :delete_all
+  has_many :likes, foreign_key: 'post_id', dependent: :delete_all
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
@@ -19,4 +19,9 @@ class Post < ApplicationRecord
   def updates_user_posts_counter
     author.increment!(:posts_counter)
   end
+
+  def less_user_posts_counter
+    author.decrement!(:posts_counter)
+  end
+
 end
