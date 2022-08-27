a5a14e7dd01d67ab8e9c7bf875cec6bb4652c947
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-27 11:35:48 -0300

    create comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,8 +1,28 @@
 class Api::CommentsController < ApplicationController
-    def index
-      @post = Post.find(params[:post_id])
-      @comments = @post.comments
-      render json: @comments
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user_id
+    comment.post_id = params[:id]
+    if comment.save
+      render json: { message: 'Yes' }, status: :ok
+    else
+      render json: { message: 'no' }, status: :400
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
   
